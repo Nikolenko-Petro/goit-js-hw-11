2d862506017e8ff110e0c98199202b6eca220339
Nikolenko-Petro <petya.dnipro@ukr.net>
2023-01-24 11:10:44 +0200

    axios

--- src/index.js
+++ src/index.js
@@ -19,13 +19,15 @@ function onBtnSearch(e) {
 
   if (inputValue !== '') {
     fetchImages(inputValue, pageNumber).then(foundData => {
-      if (foundData.hits.length === 0) {
+      if (foundData.data.hits.length === 0) {
         Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
       } else {
-        renderImageList(foundData);
-        Notiflix.Notify.success(`Hooray! We found ${foundData.total} images.`);
+        renderImageList(foundData.data.hits);
+        Notiflix.Notify.success(
+          `Hooray! We found ${foundData.data.total} images.`
+        );
         btnLoadMore.style.display = 'flex';
       }
     });
@@ -38,13 +40,15 @@ function onBtnLoadMore() {
   pageNumber++;
   const inputValue = input.value;
   fetchImages(inputValue, pageNumber).then(foundData => {
-    if (foundData.hits.length === 0) {
+    if (foundData.data.hits.length === 0) {
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
     } else {
-      renderImageList(foundData);
-      Notiflix.Notify.success(`Hooray! We found ${foundData.total} images.`);
+      renderImageList(foundData.data.hits);
+      Notiflix.Notify.success(
+        `Hooray! We found ${foundData.data.total} images.`
+      );
     }
   });
 }
@@ -52,7 +56,7 @@ function onBtnLoadMore() {
 // Cтворення розмітки
 
 function renderImageList(images) {
-  const markup = images.hits
+  const markup = images
     .map(image => {
       return `<div class="photo-card">
                <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
@@ -73,7 +77,12 @@ function renderImageList(images) {
     })
     .join('');
   gallery.insertAdjacentHTML('beforeend', markup);
+  new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+  });
 }
+
+// Слухачі
 
 btnLoadMore.addEventListener('click', onBtnLoadMore);
 btnSearch.addEventListener('click', onBtnSearch);
--- src/js/fetchImg.js
+++ src/js/fetchImg.js
@@ -1,17 +1,10 @@
+import axios from 'axios';
+
 export const fetchImages = (inputValue, pageNumber) => {
-  return fetch(
-    `https://pixabay.com/api/?key=33057198-d6ad5b417f16fab9baf60478d&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNumber}`
-  )
-    .then(response => {
-      if (!response.ok) {
-        if (response.status === 404) {
-          return [];
-        }
-        throw new Error(response.status);
-      }
-      console.log(response);
-      return response.json();
-    })
+  return axios
+    .get(
+      `https://pixabay.com/api/?key=33057198-d6ad5b417f16fab9baf60478d&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNumber}`
+    )
     .catch(error => {
       console.error(error);
     });
